ae676ab39841daa03688adabc4a0d4f24678a27d
Guy Bedford <guybedford@gmail.com>
2013-03-09 19:24:45 -0800

    AMD and browser global support

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-function heredoc(fn) {
-  return fn.toString().split('\n').slice(1,-1).join('\n') + '\n'
-}
-
-module.exports = heredoc
+(function (root, factory) {
+  if (typeof exports === 'object')
+    module.exports = factory();
+  else if (typeof define === 'function' && define.amd)
+    define(function() {
+      return factory();
+    });
+  else
+    root.heredoc = factory();
+}(this, function() {
+  return function(fn) {
+    return fn.toString().split('\n').slice(1,-1).join('\n') + '\n'
+  }
+}));
