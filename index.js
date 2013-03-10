880437a78052d0dd56687552c553ed1880bed4f8
jden <jason@denizac.org>
2013-03-09 20:36:51 -0700

    cleanup umd for readability

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-(function (root, factory) {
-  if (typeof exports === 'object')
-    module.exports = factory();
-  else if (typeof define === 'function' && define.amd)
-    define(function() {
-      return factory();
-    });
-  else
-    root.heredoc = factory();
-}(this, function() {
-  return function(fn) {
+!function (root) {
+  
+  function heredoc(fn) {
     return fn.toString().split('\n').slice(1,-1).join('\n') + '\n'
   }
-}));
+
+  if (typeof exports === 'object')
+    module.exports = heredoc
+
+  else if (typeof define === 'function' && define.amd)
+    define(function() {
+      return heredoc
+    })
+  
+  else
+    root.heredoc = heredoc
+}(this)
